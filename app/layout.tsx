87b4e5d95cf0b4de2f25b59b79403c7ed739db6b
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-07-09 07:57:57 +0530

    Project Base Ready

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import Footer from "@/components/Footer";
+
+
+export const metadata = {
+  title: "UpDriven CarHub",
+  description: "Rent Whenever you want",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
+    </html>
+  );
+}
---
+++ components/index.tsx
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Customfilter from "./CustomFilter";
+import SearchFilter from "./SearchFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./showMore";
+export { Hero, CustomButton, Footer, SearchManufacturer, Navbar, Customfilter, SearchFilter,CarCard ,CarDetails, ShowMore} 
